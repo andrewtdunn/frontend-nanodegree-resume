647abc0472f4b31e184765626b2d2ac5c5932e79
Andrew Dunn <atd2005@gmail.com>
2016-01-29 19:16:04 -0500

    refactor: adding new driver class, main.js

---
+++ js/main.js
@@ -0,0 +1,108 @@
+// init data arrays
+
+$(function(){
+    var model = {
+
+        init: function(){
+
+        },
+
+        render: function(){
+
+        }
+
+
+    };
+
+
+    var octopus = {
+
+         init: function(){
+
+        },
+
+        render: function(){
+
+        }
+
+
+    };
+
+    var headerView  = {
+
+        init: function(){
+
+        },
+
+        render: function(){
+
+        }
+
+    };
+
+    var workExperienceView = {
+
+        init: function(){
+
+        },
+
+        render: function(){
+
+        }
+
+    };
+
+    var projectsView = {
+
+        init: function(){
+
+        },
+
+        render: function(){
+
+        }
+
+    };
+
+    var educationView = {
+
+        init: function(){
+
+        },
+
+        render: function(){
+
+        }
+
+
+    };
+
+    var locationsView = {
+
+        init: function(){
+
+        },
+
+        render: function(){
+
+        }
+
+
+    };
+
+    var footerView = {
+
+        init: function(){
+
+        },
+
+        render: function(){
+
+        }
+
+
+    }
+
+    octopus.init();
+
+});
